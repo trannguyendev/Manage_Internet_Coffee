e0a2881e4e8d2323020633dda42a574cafbfc35d
Cxnnie <sephilul28@gmail.com>
2025-03-24 16:10:02 +0700

    DatDo2.0
    UI Đặt Đồ mới

--- src/DAO/DatDoDAO.java
+++ src/DAO/DatDoDAO.java
@@ -13,7 +13,8 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import Entity.DoAn;
-
+import Entity.ListDoAn;
+import Entity.ListDoUong;
 public class DatDoDAO {
     public String getPrice(String ten_sp){
         String giaSp = null;
@@ -31,4 +32,44 @@ public class DatDoDAO {
             return giaSp;
         }
     }
+    public List<ListDoAn> ListDoAn(){
+        List<ListDoAn> DoAnLst = new ArrayList<>();
+    try (Connection conn = KetNoiDB.getConnect()) {
+        String sql = "SELECT ten_sp, gia_sp FROM San_pham WHERE id_danh_muc = 2";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        while(rs.next()){
+            String ten_sp = rs.getString("ten_sp");
+            int gia_sp = rs.getInt("gia_sp");
+            ListDoAn an = new ListDoAn(ten_sp, gia_sp);
+            DoAnLst.add(an);
+        }
+       return DoAnLst;
+    } catch (Exception e) {
+        System.out.println("Error: " + e);
+        e.printStackTrace();
+        return DoAnLst;
+    }
 }
+    public List<ListDoUong> ListDoUong(){
+        List<ListDoUong> DoUongLst = new ArrayList<>();
+        try (Connection conn = KetNoiDB.getConnect()) {
+            String sql = "SELECT ten_sp, gia_sp FROM San_pham WHERE id_danh_muc = 1";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                String ten_sp = rs.getString("ten_sp");
+                int gia_sp = rs.getInt("gia_sp");
+                ListDoUong uong = new ListDoUong(ten_sp, gia_sp);
+                DoUongLst.add(uong);
+            }
+           return DoUongLst;
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+            e.printStackTrace();
+            return DoUongLst;
+        }
+    }
+    
+    }
+    
